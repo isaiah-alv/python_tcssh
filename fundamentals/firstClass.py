d68670c113e8c700f66be44975594106a0d6e2f5
isaiahAlviola <isaiahalviola0@gmail.com>
2024-10-11 20:27:52 -0400

    folders

---
+++ fundamentals/firstClass.py
@@ -0,0 +1,54 @@
+#This is a comment, a line of code that the computer does not run. 
+
+
+#Print Statement - It simply displays the text “Hello, World!” on the screen.
+
+print("Hello, World!")
+
+print("Isaiah")
+
+#Variables
+'''
+If we have to store something we need a container or box to 
+store this so, the variables are nothing but simple containers
+to store data of different types.
+
+RULES FOR NAMING!
+- must start with a letter or the underscore character.
+- cannot start with a number.
+- only contain alphabet characters, numbers and underscores (A-z, 0-9, and _ ).
+- case-sensitive (name, Name, and NAME are three different variables).
+'''
+
+name = "Isaiah"
+age = 12
+favorite_fruit = "Mangoes"
+
+
+#Data Types
+
+'''
+An integer represents whole numbers without decimals.
+'''
+age = 10
+print(age)
+
+
+'''
+A float represents numbers with decimal points
+'''
+height = 5.5
+print(height)
+
+
+'''
+A string represents a sequence of characters or alphabets.
+'''
+name = "Rahul"
+print(name)
+
+'''
+A boolean represents two possible values: True or False. It deals with only true or false. A boolean is like a switch that can be either on or off. 
+'''
+is_Delhi_Capital_of_india = True
+print(is_Delhi_Capital_of_india)
